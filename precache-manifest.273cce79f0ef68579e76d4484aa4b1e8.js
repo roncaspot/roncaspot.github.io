ba52f1a158eafeb564e9b946aedfbf4fa3bc4c6d
Yehonal <yehonal.azeroth@gmail.com>
2020-08-18 12:12:40 +0200

    Updates

---
+++ precache-manifest.273cce79f0ef68579e76d4484aa4b1e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb4e5b5a000c260bf73b2fbd1b9d4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412a5a56abfff33bb7e0",
+    "url": "/static/js/2.2efebc71.chunk.js"
+  },
+  {
+    "revision": "41973d0214293154d496",
+    "url": "/static/js/main.ca630392.chunk.js"
+  },
+  {
+    "revision": "cabdf983fafaf03c7614",
+    "url": "/static/js/runtime-main.8451cb30.js"
+  },
+  {
+    "revision": "b04cb887ee593e23881ad015d74dd71a",
+    "url": "/static/media/achi-crypto.b04cb887.jpg"
+  },
+  {
+    "revision": "c4e723afaf67e53bfd5f89feb3e2820c",
+    "url": "/static/media/achi-redsquarelab.c4e723af.jpg"
+  },
+  {
+    "revision": "1340098653e0a7c2ed889f4b76eb249d",
+    "url": "/static/media/achi-roncaspot.13400986.png"
+  },
+  {
+    "revision": "52ec11ba529da60fe2b93abfa6271ed6",
+    "url": "/static/media/avatar.52ec11ba.png"
+  },
+  {
+    "revision": "5eb4abecb3743620d1ed86d0e5b2d49c",
+    "url": "/static/media/big_avatar.5eb4abec.jpg"
+  },
+  {
+    "revision": "d5cf61bf6ef17db045ef1d0ef71d54ef",
+    "url": "/static/media/home_bg.d5cf61bf.jpg"
+  },
+  {
+    "revision": "7da84724e988fcc28c7df6e275429814",
+    "url": "/static/media/sign.7da84724.png"
+  }
+]);
